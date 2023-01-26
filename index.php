c6ff4a52e955c7b066e558d8fb13ea9dba28d255
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-01-26 19:35:11 +0100

    Imported file index.php and created input form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+</head>
+
+<body>
+    <main>
+        <form action="./index.php" method="GET">
+            <label for="password_length">Lunghezza password:</label>
+            <input type="text" name="password_length">
+            <button type="submit">Genera password</button>
+        </form>
+        <?php
+
+        ?>
+    </main>
+</body>
+
+</html>
