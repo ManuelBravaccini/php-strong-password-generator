2d2581acd6c67938258ba0b03f00e221dfe0aaf9
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-01-26 20:04:56 +0100

    Password generator with length chosen by user

--- index.php
+++ index.php
@@ -15,8 +15,19 @@
             <input type="text" name="password_length">
             <button type="submit">Genera password</button>
         </form>
-        <?php
 
+        <?php
+        $length = (int) $_GET['password_length'];
+
+        function generatePassword($length)
+        {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+            return substr(str_shuffle($chars), 0, $length);
+        }
+
+        $password = generatePassword($length);
+
+        echo "La tua password è: " . $password;
         ?>
     </main>
 </body>
